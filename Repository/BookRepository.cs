8015b5b7b1e641eb62cdbf440f96a19daee7c7ed
Atanas Gyulchev <gyulchev666@gmail.com>
2026-08-09 20:05:57 +0300

    Add the publisher and book repositories
    Derive both repositories from the generic base so they inherit the shared data
    access operations and pass the context down to it.

---
+++ Repository/BookRepository.cs
@@ -0,0 +1,12 @@
+using Contracts;
+using Entities.Models;
+
+namespace Repository;
+
+public class BookRepository : RepositoryBase<Book>, IBookRepository
+{
+    public BookRepository(RepositoryContext repositoryContext)
+        : base(repositoryContext)
+    {
+    }
+}
---
+++ Repository/PublisherRepository.cs
@@ -0,0 +1,12 @@
+using Contracts;
+using Entities.Models;
+
+namespace Repository;
+
+public class PublisherRepository : RepositoryBase<Publisher>, IPublisherRepository
+{
+    public PublisherRepository(RepositoryContext repositoryContext)
+        : base(repositoryContext)
+    {
+    }
+}
